c869c330a4236aa8b229d79bbf920f9e731cd572
ussraa <92607242+youssra-ghninou@users.noreply.github.com>
2023-06-02 00:52:30 +0100

    notes fetched and created successfully

---
+++ app/components/Details.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+export default function Details() {
+  return (
+    <View>
+      <Text>Details</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({})
